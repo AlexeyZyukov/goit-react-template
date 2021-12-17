3d71c1478778aff17503a4319f06e7200c19fdb8
Alexey Zyukov <aalex64@ukr.net>
2021-12-17 11:39:06 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  // Объявляем новую переменную состояния "count"
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Обновляем заголовок документа, используя API браузера
+    document.title = `Вы нажали ${count} раз`;
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Вы нажали {count} раз</p>
+      <button onClick={() => setCount(count + 1)}>Нажми на меня</button>
     </div>
   );
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
